de72251c27406d3dafec20cd6d4225e55f22bf15
xxchan <xxchan@sjtu.edu.cn>
2020-05-02 15:18:21 +0800

    implement some node functions

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -8,12 +8,25 @@ use crypto::digest::Digest;
 
 
 #[derive(Serialize, Deserialize)]
-struct Block {
+pub struct Block {
     index: u64,
     timestamp: SystemTime,
     proof: u64,
     transactions: Vec<Transaction>,
     previous_hash: String,
+}
+
+impl Block {
+    pub fn get_proof(&self) -> u64 { self.proof }
+
+    pub fn get_index(&self) -> u64 { self.index }
+    /// Hashes a Block
+    pub fn get_hash(&self) -> String {
+        let block_string = serde_json::to_string(self).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.input_str(&block_string);
+        hasher.result_str()
+    }
 }
 
 pub struct Blockchain {
@@ -27,7 +40,7 @@ impl Blockchain {
     pub fn new() -> Self { unimplemented!() }
 
     /// Creates a new Block and adds it to the chain
-    pub fn new_block(&mut self, proof: u64, previous_hash: String) {
+    pub fn new_block(&mut self, proof: u64, previous_hash: String) -> &Block {
         let transactions = mem::replace(&mut self.current_transactions, Vec::new());
 
         let block = Block {
@@ -39,29 +52,28 @@ impl Blockchain {
         };
 
         self.blocks.push(block);
+        self.last_block()
     }
 
     /// Adds a new transaction to the list of transactions.
     /// Returns the index of the Block that will hold this transaction
-    pub fn new_transaction(&mut self, sender: String, recipient: String, amount: i64) -> u64 {
-        self.current_transactions.push(Transaction { sender, recipient, amount });
+    pub fn new_transaction(&mut self, sender: &str, recipient: &str, amount: i64) -> u64 {
+        self.current_transactions.push(Transaction {
+            sender: sender.to_owned(),
+            recipient: recipient.to_owned(),
+            amount,
+        });
         (self.blocks.len() + 1) as u64
     }
 
-    /// Hashes a Block
-    fn hash(block: &Block) -> String {
-        let block_string = serde_json::to_string(block).unwrap();
-        let mut hasher = Sha256::new();
-        hasher.input_str(&block_string);
-        hasher.result_str()
-    }
 
     /// Returns the last Block in the chain
-    fn last_block(&self) -> &Block {
+    pub fn last_block(&self) -> &Block {
         &self.blocks.last().unwrap()
     }
 
-    fn proof_of_work(last_proof: u64) -> u64 {
+    /// Proof of Work algorithm
+    pub fn proof_of_work(last_proof: u64) -> u64 {
         let mut proof = 0;
         while Blockchain::valid_proof(last_proof, proof) == false {
             proof += 1;
--- src/node.rs
+++ src/node.rs
@@ -2,6 +2,7 @@
 use crate::Blockchain;
 
 struct Node {
+    index: u64,
     chain: Blockchain,
 }
 
@@ -9,10 +10,23 @@ impl Node {
     pub fn new() -> Self { unimplemented!() }
 
     /// Mines a new block
-    pub fn mine() { unimplemented!() }
+    pub fn mine(&mut self) {
+        let last_block = self.chain.last_block();
+        let proof = Blockchain::proof_of_work(last_block.get_proof());
+        let last_hash = last_block.get_hash();
+        // receive a reward for finding the proof.
+        // The sender is "0" to signify that this node has mined a new coin.
+        self.new_transaction("0", &self.index.to_string(), 1);
+
+        let block = self.chain.new_block(proof, last_hash);
+        info!("[Node {}] A new block {} is forged", self.index, block.get_index());
+    }
 
     /// Adds a new transaction
-    pub fn new_transaction() { unimplemented!() }
+    pub fn new_transaction(&mut self, sender: &str, receiver: &str, amount: i64) {
+        self.chain.new_transaction(sender, receiver, amount);
+        info!("[Node {}] A new transaction is added: {} -> {}, amount: {}", self.index, sender, receiver, amount);
+    }
 
     /// Displays the full blockchain
     pub fn display() { unimplemented!() }
